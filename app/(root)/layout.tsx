d7e02ff3af2e0108f5b82f417916a6fc12c19ac6
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2025-01-07 16:54:52 +0200

    initial project set-up

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function RootLayout({children,}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="flex flex-col h-screen">
+            <main className="flex-1 wrapper">
+                {children}
+            </main>
+        </div>
+    );
+}
